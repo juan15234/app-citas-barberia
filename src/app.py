93c073382a8fec4f472707f4161f439079c4b363
juan_cano <75386701+juan15234@users.noreply.github.com>
2025-06-09 12:13:24 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -22,71 +22,24 @@ connection = mysql.connector.connect(
 
 @app.route('/')
 def index():
-    return redirect(url_for('registro'))
-
-@app.route('/registro',methods=['POST', 'GET'])
-def registro():
-    if request.method == 'POST':
-        
-        user = User(
-        nombre = request.form['nombre'],
-        contraseña = request.form['contraseña'],
-        email = request.form['email'],
-        numero_tel = request.form['telefono']
-        )
-        
-        registro_user = UserModel.registro(connection,user)
-        print(registro_user)
-        
-        if registro_user == 'usuario creado con exito':
-            return redirect(url_for('home'))
-        else:
-            mensaje = registro_user
-            return render_template('registro.html', mensaje=mensaje)
-    
-    else:
-        return render_template('registro.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    
-    if request.method == 'POST':
-        
-        user = (
-            request.form['nombre'],
-            request.form['contraseña'],
-        )
-
-        login_user = UserModel.login(connection, user)
-        
-        if login_user == 'usuario encontrado':
-            return redirect(url_for('home'))
-        else:
-            mensaje = login_user
-            return render_template('registro.html', mensaje=mensaje)
-    
-    else:
-        return render_template('registro.html')
-    
+    return redirect(url_for('home'))
 #---------------------home---------------------------
 
 @app.route('/home')
 def home():
-    
-    nombre = session['nombre']
-    
-    return f'Hola {nombre}'
+    return render_template('home.html)
 
 @app.route('/citas')
 def view_citas():
     
     cita = Cita(
-        nombre='samuel',
-        email='nose@gmail.com',
-        numero_tel='3161827643',
-        tipo_servicio='barba',
-        barbero='marcos',
-        fecha_hora='2025-06-10T14:30:00'
+        nombre= request.form['nombre'],
+        email=request.form['email'],
+        numero_tel=request.form['telefono'],
+        tipo_servicio=request.form['servicio'],
+        barbero=request.form['barbero'],
+        fecha=request.form['fecha'],
+        hora=request.form['hora']
     )
     
     citas = CitaModel.view_citas(connection,cita)
