e10f7116248414625bc17877b9ee8b13106eed22
juan_cano <75386701+juan15234@users.noreply.github.com>
2025-06-09 12:03:08 -0500

    Update User.py

--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -1,18 +1,7 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-
 class User:
     
-    def __init__(self,nombre,contraseña,email,numero_tel):
+    def __init__(self,nombre,email,numero_tel):
         self.nombre=nombre
-        self.contraseña=contraseña
         self.email=email
         self.numero_tel=numero_tel
-        
-    @classmethod
-    def revisar_hashed_password(cls, hashed_password, password):
-        return check_password_hash(hashed_password, password)
-    
-    @classmethod
-    def generar_hashed_password(cls, password):
-        return generate_password_hash(password)
         
