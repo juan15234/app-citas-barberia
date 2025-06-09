27b725a3582eeffa2008e537f302f7a6181c3768
juan_cano <75386701+juan15234@users.noreply.github.com>
2025-06-08 21:14:35 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -110,4 +110,4 @@ def crear_cita():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',  port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0',  port=int(os.environ.get("MYSQLPORT", 5000)), debug=True)
