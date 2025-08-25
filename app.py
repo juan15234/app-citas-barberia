36fd05e8cf77bb1a6e790387c37f467bfbf64f89
juan_cano <75386701+juan15234@users.noreply.github.com>
2025-08-25 13:10:41 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,174 @@
+from flask import Flask, redirect, render_template, request, jsonify, session, url_for
+import uuid
+import os
+from dotenv import load_dotenv
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from config import config
+from models.CitaModel import CitaModel
+from models.GoogleCalendar import GoogleCalendar
+from models.Correo import Correo
+from conexion import obtener_conexion
+
+load_dotenv(dotenv_path=".env")
+
+app = Flask(__name__)
+app.config.from_object(config['development'])
+app.secret_key = os.getenv('SECRET_KEY')
+
+@app.route('/horas-disponibles')
+def horas_disponibles():
+    fecha = request.args.get("fecha")
+    barbero = request.args.get("barbero")
+    
+    barbero_numero = CitaModel.barbero_numero(barbero)
+    
+    horas_disponibles = CitaModel.horas_disponibles( fecha, barbero_numero)
+    
+    return jsonify(horas_disponibles)
+
+
+#------------------------------------
+@app.route('/crear_cita')
+def crear_cita():
+    try:
+        
+        nombre = request.args.get('nombre')
+        correo_cliente = request.args.get('correo_cliente')
+        telefono_cliente = request.args.get('telefono_cliente')
+        barbero = request.args.get('barbero')
+        fecha = request.args.get('fecha')
+        hora = request.args.get('hora')
+        servicio_numero = session.get('servicio_numero')
+        duracion = session.get('duracion')
+        nota_cliente = request.args.get('nota')
+
+        barbero_numero = CitaModel.barbero_numero(barbero)
+
+        session['nombre'] = nombre
+        session['barbero'] = barbero
+        session['hora'] = hora
+        session['fecha'] = fecha
+        session['correo_cliente'] = correo_cliente
+        session['telefono_cliente'] = telefono_cliente
+        session['nota_cliente'] = nota_cliente
+        
+        token = str(uuid.uuid4())
+
+        crear_cita = CitaModel.crear_cita(nombre, barbero_numero, fecha, hora, correo_cliente, telefono_cliente, servicio_numero, duracion, token, nota_cliente)
+
+        if crear_cita == 'Cita creada con exito':
+            
+            return jsonify({'ok': True, 'estatus': crear_cita, 'token': token, 'fecha':fecha})
+        else:
+            return jsonify({'ok':False, 'estatus': crear_cita, 'fecha':fecha})
+        
+    except Exception as e:
+        print(e)
+
+
+@app.route('/agendar/<servicio>')
+def agendar(servicio):
+
+    GoogleCalendar.citas_en_calendario()
+    
+    session['servicio'] = servicio
+    
+    servicio_numero, duracion = CitaModel.servicio_numero(servicio)
+    session['servicio_numero'] = servicio_numero
+    session['duracion'] = duracion
+    return render_template("agendar.html")
+
+@app.route('/programar_cita_calendario_y_enviar_correo')
+def programar_cita_calendario_y_enviar_correo():
+    
+    nombre = session.get('nombre')
+    servicio = session.get('servicio')
+    barbero = session.get('barbero')
+    duracion = session.get('duracion')
+    hora = session.get('hora')
+    fecha = session.get('fecha')
+    correo_cliente = session.get('correo_cliente')
+    servicio_numero = session.get('servicio_numero')
+    telefono_cliente = session.get('telefono_cliente')
+    nota_cliente = session.get('nota_cliente')
+    
+    token = request.args.get('token')
+    
+    evento = GoogleCalendar.crear_evento(nombre, servicio, barbero, correo_cliente, telefono_cliente, hora, fecha, duracion, nota_cliente)
+    print(evento)
+    
+    enviar_correo = Correo.enviar_correo(correo_cliente, nombre, barbero, hora, fecha, servicio_numero, token)
+    print(enviar_correo)
+    
+    guardar_eventos_en_bd = GoogleCalendar.citas_en_calendario()
+    print(guardar_eventos_en_bd)
+    
+    session.clear()
+    
+    return jsonify({'evento':evento, 'enviar_correo':enviar_correo, 'guardar_eventos_en_bd':guardar_eventos_en_bd})
+
+@app.route('/editar_cita/<token>', methods=['GET'])
+def editar_cita(token):
+    
+    session['token'] = token
+    
+    
+    return render_template('editar_cita.html')
+
+@app.route('/editar_cita_backend', methods=['GET', 'POST'])
+def editar_cita_backend():
+    nueva_fecha = request.args.get('nueva_fecha')
+    nueva_hora = request.args.get('nueva_hora')
+    nuevo_barbero = request.args.get('nuevo_barbero')
+    token = session.get('token')
+    
+    nuevo_barbero = CitaModel.barbero_numero(nuevo_barbero)
+    
+
+    cita_editada = CitaModel.editar_cita(nueva_fecha, nueva_hora, nuevo_barbero, token)
+    
+    evento_editado = GoogleCalendar.editar_evento(token, nuevo_barbero, nueva_hora, nueva_fecha)
+    
+    print(f'{cita_editada}, {evento_editado}')
+    
+    if cita_editada == 'cita editada exitosamente' and evento_editado == 'evento editado exitosamente':
+            
+        return jsonify({'ok': True, 'estatus_bd': cita_editada, 'estatus_google_calendar':evento_editado,'token': token})
+    else:
+        return jsonify({'ok':False, 'estatus_bd': cita_editada, 'estatus_google_calendar':evento_editado})
+
+@app.route('/cancelar_cita_backend')
+def cancelar_cita_backend():
+    token = session.get('token')
+    
+    cita_cancelada = CitaModel.cancelar_cita(token)
+    
+    evento_cancelado = GoogleCalendar.eliminar_evento(token)
+    
+    print(f'{cita_cancelada}, {evento_cancelado}')
+    
+    session.clear()
+    
+    if cita_cancelada == 'cita cancelada exitosamente' and evento_cancelado == 'evento eliminado exitosamente':
+        return jsonify({'ok': True, 'estatus_bd': cita_cancelada, 'estatus_google_calendar':evento_cancelado,'token': token})
+    else:
+        return jsonify({'ok':False, 'estatus_bd': cita_cancelada, 'estatus_google_calendar':evento_cancelado})
+
+@app.route('/home')
+def home():
+    
+    return render_template('home.html')
+
+@app.route('/')
+def main():
+    return redirect(url_for('home'))
+
+#ENVIO DE CORREOS DE RECORDATORIO DE MANERA AUTOMATICA
+scheduler = BackgroundScheduler()
+scheduler.add_job(Correo.enviar_recordatorio, trigger='cron', hour=9, misfire_grace_time=3600)
+scheduler.add_job(CitaModel.eliminar_citas_viejas, trigger='cron', hour=9, misfire_grace_time=3600)
+scheduler.start()
+
+if __name__ == '__main__':
+    app.run()
---
+++ models/GoogleCalendar.py
@@ -0,0 +1,267 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from datetime import datetime, timedelta
+import uuid
+import os
+import json
+import base64
+
+from models.CitaModel import CitaModel
+from conexion import obtener_conexion
+
+class GoogleCalendar:
+
+    @classmethod
+    def crear_evento(cls, nombre, servicio, barbero, correo_cliente, telefono_cliente, hora, fecha, duracion, nota_cliente):
+        
+        try:
+            
+            barbero = barbero.lower()
+            
+            if barbero == 'william':
+                colorId = '3'
+            
+            elif barbero == 'ruben':
+                colorId = '4'
+                
+            elif barbero == 'bryan':
+                colorId = '6'
+                
+            elif barbero == 'marcos':
+                colorId = '1'
+                
+            elif barbero == 'jhon':
+                colorId = '2'
+            
+            credentials_json = base64.b64decode(os.getenv('GOOGLE_CREDENTIALS_BASE64')).decode('utf-8')
+            info = json.loads(credentials_json)
+            
+            SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+            CALENDAR_ID = os.getenv('CORREO_BARBERIA')
+            
+            inicio_str = f'{fecha}T{hora}:00'
+            fecha_ini = datetime.strptime(inicio_str, '%Y-%m-%dT%H:%M:%S')
+
+            fecha_fin = fecha_ini + timedelta(minutes=duracion)
+
+            credentials = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+
+            service = build('calendar', 'v3', credentials=credentials, cache_discovery=False)
+            
+
+            evento = {
+                'summary':f'nombre: {nombre}',
+                'description': f'servicio: {servicio} | barbero: {barbero} | correoCliente: {correo_cliente} | telefonoCliente:  {telefono_cliente} | NotaCliente: {nota_cliente}',
+                'start': {
+                    'dateTime': fecha_ini.isoformat(),
+                    'timeZone': 'America/Bogota',
+                },
+                'end':{
+                    'dateTime': fecha_fin.isoformat(),
+                    'timeZone': 'America/Bogota',
+                },
+                'colorId': colorId
+            }
+
+            evento = service.events().insert(calendarId=CALENDAR_ID, body=evento).execute()
+        except Exception as e:
+            print(e)
+            
+    @classmethod
+    def citas_en_calendario(cls):
+            
+        def extraer_campo(campo, texto):
+            if campo in texto:
+                partes = texto.split(campo + ':')
+                return partes[1].split('|')[0].strip()
+            return 'Desconocido'
+        
+        credentials_json = base64.b64decode(os.getenv('GOOGLE_CREDENTIALS_BASE64')).decode('utf-8')
+        info = json.loads(credentials_json)
+        
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        CALENDAR_ID = os.getenv('CORREO_BARBERIA')
+        credentials = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+        service = build('calendar', 'v3', credentials=credentials, cache_discovery=False)
+        
+        now = datetime.utcnow().isoformat() + 'Z'
+        
+        events_results = service.events().list(calendarId=CALENDAR_ID, timeMin=now, singleEvents=True, orderBy='startTime').execute()
+        events = events_results.get('items', [])
+        
+        for event in events:
+            try:
+                event_summary = event.get('summary', '')
+                event_description = event.get('description', '')
+
+                fecha_hora = datetime.fromisoformat(event['start']['dateTime'])
+
+                usuario = extraer_campo('nombre', event_summary)
+
+                email = extraer_campo('correoCliente', event_description)
+                telefono = extraer_campo('telefonoCliente', event_description)
+                nota_cliente = extraer_campo('notaCliente', event_description)
+
+                servicio, duracion = CitaModel.servicio_numero(extraer_campo('servicio', event_description))
+
+                barbero = CitaModel.barbero_numero(extraer_campo('barbero', event_description))
+
+                sql="""SELECT usuario FROM citas WHERE barbero=%s AND fecha_hora=%s"""
+                values = (barbero, fecha_hora)
+
+                conexion = obtener_conexion()
+                cursor = conexion.cursor()
+
+                cursor.execute(sql, values)
+
+                resultado = cursor.fetchone()
+
+                token = str(uuid.uuid4())
+
+
+                if resultado:
+                    print('evento ya existente en bd')
+                else:
+                    sql="""INSERT INTO citas(usuario, barbero, fecha_hora, correo_cliente, telefono_cliente, servicio_numero, duracion, token, nota_cliente) VALUES (%s,%s,%s,%s,%s,%s,%s, %s, %s)"""
+                    values = (usuario, barbero, fecha_hora, email, str(telefono), servicio, duracion,  token, nota_cliente)
+                    conexion = obtener_conexion()
+                    cursor = conexion.cursor()
+                    cursor.execute(sql, values)
+                    conexion.commit()
+
+                    print('evento guardado en bd')
+            except Exception as e:
+                print(e)
+        return 'citas de google calendar guardadas en base de datos'
+            
+            
+    @classmethod
+    def editar_evento(cls, token, nuevo_barbero, nueva_hora, nueva_fecha):
+        
+        def extraer_campo(campo, texto):
+                if campo in texto:
+                    partes = texto.split(campo + ':')
+                    return partes[1].split('|')[0].strip()
+                return 'Desconocido'
+        
+        credentials_json = base64.b64decode(os.getenv('GOOGLE_CREDENTIALS_BASE64')).decode('utf-8')
+        info = json.loads(credentials_json)
+        
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        
+        CALENDAR_ID = os.getenv('CORREO_BARBERIA')
+        
+        credentials = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+        
+        
+        service = build('calendar', 'v3', credentials=credentials, cache_discovery=False)
+
+        now = datetime.utcnow().isoformat() + 'Z'
+        
+        sql="""SELECT usuario FROM citas WHERE token=%s"""
+        values = (token)
+        
+        conexion = obtener_conexion()
+        cursor = conexion.cursor()
+        cursor.execute(sql, values)
+        
+        resultado = cursor.fetchone()
+        
+        usuario = resultado[0]
+        
+
+        events_results = service.events().list(calendarId=CALENDAR_ID, timeMin=now, singleEvents=True, orderBy='startTime').execute()
+
+        events = events_results.get('items', [])
+
+        for event in events:
+            
+            event_summary = event.get('summary', '')
+            event_description = event.get('description', '')
+            
+            if event_summary == f'Nombre: {usuario}':
+                
+                event_id = event['id']
+                
+                servicio = extraer_campo('servicio', event_description)
+                correo_cliente = extraer_campo('Correo_cliente', event_description)
+                telefono_cliente = extraer_campo('Telefono_cliente', event_description)
+                
+                fecha_ini = datetime.fromisoformat(event['start']['dateTime'])
+                fecha_fin = datetime.fromisoformat(event['end']['dateTime'])
+                
+                duracion = int((fecha_fin - fecha_ini).total_seconds() / 60)
+                
+                fecha_str = f'{nueva_fecha}T{nueva_hora}:00'
+                
+                nueva_fecha_ini = datetime.strptime(fecha_str, '%Y-%m-%dT%H:%M:%S')
+                nueva_fecha_fin = nueva_fecha_ini + timedelta(minutes=duracion)
+                
+                colorId = event['colorId']
+                
+                evento_editado = {
+                        'summary':f'Nombre: {usuario}',
+                        'description': f'Servicio: {servicio} | Barbero: {nuevo_barbero} | Correo_cliente: {correo_cliente} | Telefonor_cliente:  {telefono_cliente}',
+                        'start': {
+                            'dateTime': nueva_fecha_ini.isoformat(),
+                            'timeZone': 'America/Bogota',
+                        },
+                        'end':{
+                            'dateTime': nueva_fecha_fin.isoformat(),
+                            'timeZone': 'America/Bogota',
+                        },
+                        'colorId': colorId
+                    }
+                
+                evento_editado = service.events().patch(calendarId=CALENDAR_ID, eventId=event_id, body=evento_editado).execute()
+                print(evento_editado)
+            else:
+                print('evento no encontrado')
+                
+        return 'evento editado exitosamente'
+            
+    @classmethod
+    def eliminar_evento(cls, token):
+        
+        credentials_json = base64.b64decode(os.getenv('GOOGLE_CREDENTIALS_BASE64')).decode('utf-8')
+        info = json.loads(credentials_json)
+        
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        
+        CALENDAR_ID = os.getenv('CORREO_BARBERIA')
+        
+        credentials = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+        
+        
+        service = build('calendar', 'v3', credentials=credentials, cache_discovery=False)
+
+        now = datetime.utcnow().isoformat() + 'Z'
+        
+        sql="""SELECT usuario FROM citas WHERE token=%s"""
+        
+        values = (token)
+        
+        conexion = obtener_conexion()
+        cursor = conexion.cursor()
+        cursor.execute(sql, values)
+        
+        usuario = cursor.fetchone()
+        
+
+        events_results = service.events().list(calendarId=CALENDAR_ID, timeMin=now, singleEvents=True, orderBy='startTime').execute()
+
+        events = events_results.get('items', [])
+
+        for event in events:
+            
+            event_summary = event.get('summary', '')
+            
+            if event_summary == f'Nombre: {usuario}':
+                
+                event_id = event['id']
+        
+                evento_eliminado = service.events().delete(calendarId=CALENDAR_ID, eventId=event_id).execute()
+                print(evento_eliminado)
+                
+        return 'evento eliminado exitosamente'
